2c0177230418ae7704c8f075bdc2786b77d1986a
Ricard Garcia <imricardgarcia@gmail.com>
2021-05-19 16:32:05 +0200

    Commit Ricard

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <main className="container mt-5">
       <section className="row">
         <div className="col col-12">
-          <h1>Hola mundo</h1>
+          <h1>Hola mundo editado</h1>
         </div>
       </section>
     </main>
