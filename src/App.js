efd62ae9d2bad332114e66d53fdd6319de676d62
김은영 <solaz0824@gmail.com>
2021-05-19 15:15:50 +0200

    App.js class made

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 // import pages
@@ -10,14 +10,22 @@ import Active from "./pages/Active";
 // import style
 import "./App.scss";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Route path="/" render={() => <Home />} />
-      <Route path="/completed" render={() => <Completed />} />
-      <Route path="/active" render={() => <Active />} />
-    </BrowserRouter>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" render={() => <Home />} />
+        <Route path="/completed" render={() => <Completed />} />
+        <Route path="/active" render={() => <Active />} />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
