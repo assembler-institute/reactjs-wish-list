e62f1b0823a9bd23d6b79285beafdd5ae8b9b022
Brahim <brahimcasas@hotmail.es>
2021-05-19 16:30:38 +0200

    react jsx structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
+import hero from "./img/hero.jpg";
+import "./header.scss";
+import "./main.scss";
 
 function App() {
   return (
-    <main className="container mt-5">
-      <section className="row">
-        <div className="col col-12">
-          <h1>Hola mundo</h1>
-        </div>
-      </section>
-    </main>
+    <>
+      <header>
+        <img alt="hero" src={hero} />
+        <h1>TODO</h1>
+        <form>
+          <input type="text" placeholder="Create task" />
+        </form>
+      </header>
+      <main className="container mt-5">
+        <section className="row">
+          <div className="col col-6">
+            <label>
+              <input type="checkbox" name="name" />
+              Jog around the park
+            </label>
+          </div>
+          <div className="col col-6">
+            <label>
+              <input type="checkbox" name="name" />
+              Jog around the park
+            </label>
+          </div>
+          <div className="col col-6">
+            <label>
+              <input type="checkbox" name="name" />
+              Jog around the park
+            </label>
+          </div>
+        </section>
+        <section>
+          <span>5 items left</span>
+          <button type="button">All</button>
+          <button type="button">Active</button>
+          <button type="button">Completed</button>
+          <button type="button">Clear completed</button>
+        </section>
+      </main>
+    </>
   );
 }
 
