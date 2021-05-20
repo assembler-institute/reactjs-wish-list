b80c35c1375c47c6a77697c1374754f0f1002872
Nachomontoya <montoyanacho@gmail.com>
2021-05-20 11:14:11 +0200

    component Main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Main.scss";
+import mountainImg from "../../img/mountain.jpg";
+
+function Main() {
+  return (
+    <main className="fullpage position-relative">
+      <div className="fullcontent">
+        <div className="container-fluid, h40">
+          <div className="gradient" />
+          <img src={mountainImg} alt="Mountain background" />
+        </div>
+        <div className="container-fluid h60 bg-light" />
+      </div>
+      <section className="container-sm position-absolute d-flex flex-column p-0 mainContent">
+        <header className="d-flex flex-row justify-content-between mb-5">
+          TODO
+        </header>
+        <aside className="bg-light d-flex flex-row rounded p-3 mb-4">
+          Input
+        </aside>
+        <article className="bg-light d-flex flex-column shadow rounded toContent p-3">
+          TODO LIST
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/pages/TestDesign/TestDesign.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Main from "../../components/Main";
+
+function TestDesign() {
+  return <Main />;
+}
+
+export default TestDesign;
