abbcb6896e60362bdee2d0eaa089dfb48e391a8c
Armando Vieira <80274998+armandovieirag@users.noreply.github.com>
2021-05-20 11:11:13 +0200

    ready

--- src/components/TodosContainer/TodosContainer.js
+++ src/components/TodosContainer/TodosContainer.js
@@ -4,15 +4,22 @@ import "./TodosContainer.scss";
 
 function TodosContainer() {
   return (
-    <div className="input-group borders">
-      <div className="">
-        <input className="checkbox-round" type="checkbox" />
+    <>
+      <div className="input-group borders">
+        <div className="">
+          <input className="checkbox-round" type="checkbox" />
+        </div>
+        <input className="form-control" type="text" />
+        <button type="button" className="btn cancel" />
       </div>
-      <input className="form-control" type="text" />
-      <button type="button" className="btn">
-        X
-      </button>
-    </div>
+      <div className="input-group borders">
+        <div className="">
+          <input className="checkbox-round" type="checkbox" />
+        </div>
+        <input className="form-control" type="text" />
+        <button type="button" className="btn cancel" />
+      </div>
+    </>
   );
 }
 
