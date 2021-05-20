66ae18010cbf382c8ad42e4d715e36d46239b4a9
Ricard Garcia <imricardgarcia@gmail.com>
2021-05-20 12:53:14 +0200

    Added default todos

---
+++ src/utils/demo-data.js
@@ -0,0 +1,20 @@
+// This file is used to render default todo objects
+const defaultTodos = [
+  {
+    id: "65d6b269-209b-50ac-a566-ca6a334aa6f0",
+    text: "Washing dishes",
+    done: false,
+  },
+  {
+    id: "63d6b269-300b-50ac-a566-ca6a334aa6f0",
+    text: "Make presentation",
+    done: false,
+  },
+  {
+    id: "62d6b269-200b-50ac-a566-ca6a334aa6f0",
+    text: "Be cool",
+    done: true,
+  },
+];
+
+export default defaultTodos;
