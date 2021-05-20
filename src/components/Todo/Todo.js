29e44a577734b40f28543b95284a1183c98086e3
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-05-20 17:53:29 +0200

    Trying to print todos

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./_Todo.scss";
+import TodoItem from "../TodoItem";
 
-function Todo() {
+function Todo({ todos }) {
+  console.log(todos);
   return (
-    <>
-      <div className="main-todo-container" />
-    </>
+    <div className="main-todo-container">
+      <ul className="todo_print">
+        <TodoItem />
+      </ul>
+    </div>
   );
 }
-
 export default Todo;
---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoItem = () => {
+  return (
+    <div>
+      <li>JON</li>
+    </div>
+  );
+};
+
+export default TodoItem;
