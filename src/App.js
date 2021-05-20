d0c0d5e8ffb6ab9386478ef70e118c6867cc6fc2
Brahim <brahimcasas@hotmail.es>
2021-05-20 11:26:35 +0200

    main styles finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import hero from "./img/hero.jpg";
 import "./header.scss";
 import "./main.scss";
@@ -23,18 +24,21 @@ function App() {
               <input className="mx-4" type="checkbox" name="name" />
               Jog around the park
             </label>
+            <AiOutlineClose className="main__todo__close" size={24} />
           </div>
           <div className="main__todo col col-12">
             <label>
               <input className="mx-4" type="checkbox" name="name" />
               Jog around the park
             </label>
+            <AiOutlineClose className="main__todo__close" size={24} />
           </div>
           <div className="main__todo col col-12">
             <label>
               <input className="mx-4" type="checkbox" name="name" />
               Jog around the park
             </label>
+            <AiOutlineClose className="main__todo__close" size={24} />
           </div>
         </section>
         <section className="row main__footer">
