5427a90fc207bf399f152dd60e9159a4d52a9858
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-05-20 09:48:10 +0200

    pull

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.scss";
 
 import Header from "./components/Header";
 import NewTodo from "./components/NewTodo";
-import TodoList from "./components/TodoList";
+import TodoList from "./components/todolist";
 
 import * as api from "./api";
 
