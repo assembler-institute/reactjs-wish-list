36cf745abc6af94e5e9dec9a336fd50d2e6f4734
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-05-20 11:08:13 +0200

    trying form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React from "react";
-import Form from "./components/Form/Form";
+import React, { Component } from "react";
+import Form from "./components/Form";
 import Header from "./components/Header/Header";
 import Todo from "./components/Todo";
 import "./_App.scss";
 
-function App() {
-  return (
-    <main className="background_container">
-      <Header />
-      <section className="parent_container">
-        <h1>TODO</h1>
-        <Form />
-        <Todo />
-      </section>
-    </main>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
+    this.newTodo = this.newTodo.bind(this);
+  }
 
+  newTodo(titl) {
+    const { todos } = this.state;
+    // eslint-disable-next-line
+    console.log("empty", todos);
+    todos.push(titl);
+    this.setState({
+      todos: todos,
+    });
+    // eslint-disable-next-line
+    console.log("some", todos);
+  }
+
+  render() {
+    const { todo } = this.state;
+    return (
+      <main className="background_container">
+        <Header />
+        <section className="parent_container">
+          <h1>TODO</h1>
+          <Form />
+          <Todo toDo={todo} />
+        </section>
+      </main>
+    );
+  }
+}
 export default App;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,40 @@
-import React from "react";
 import "./_Form.scss";
+import React, { Component } from "react";
 
-function Form() {
-  return <input type="text" className="form_input" />;
+class newForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    const { title } = this.state;
+    // eslint-disable-next-line
+    console.log(this.props);
+    // newTodo(title);
+    this.setState({
+      title: event.target.value,
+    });
+    // eslint-disable-next-line
+    console.log(title);  
+  }
+
+  render() {
+    const { title } = this.state;
+    return (
+      <>
+        <input
+          type="text"
+          className="form_input"
+          value={title}
+          onChange={this.handleInput}
+        />
+        <button type="button" onClick={this.newTodo} />
+      </>
+    );
+  }
 }
-
-export default Form;
+export default newForm;
