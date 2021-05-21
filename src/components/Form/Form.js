ee0e1493b7ce02d4886622596e19d8aed2f3c8ae
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-05-21 17:16:38 +0200

    Branch finished

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,57 +5,57 @@ class newForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todoIt: {
-        title: "",
-        id: 0,
-        selected: false,
-      },
+      title: "",
+      id: 0,
+      selected: false,
     };
     this.handleInput = this.handleInput.bind(this);
     this.submitTitle = this.submitTitle.bind(this);
   }
 
   handleSelected() {
-    const { todoIt } = this.state;
-    todoIt.selected = true;
+    let { selected } = this.state;
+    selected = true;
     this.setState({
-      todoIt: todoIt,
+      selected: selected,
     });
     // eslint-disable-next-line
-    console.log("Este es el valor", todoIt.selected);
+    console.log("Este es el valor", selected);
   }
 
   handleInput(event) {
-    const { todoIt } = this.state;
-    todoIt.title = event.target.value;
+    let { title } = this.state;
+    title = event.target.value;
     this.setState({
-      todoIt: todoIt,
+      title: title,
     });
     // eslint-disable-next-line
-    console.log("Este es titile", todoIt.title);
+    console.log("Este es titile", title);
   }
 
   submitTitle(event) {
     if (event.key === "Enter") {
-      const { ...todoIt } = this.state;
+      const { ...newTodos } = this.state;
       const { newTodo } = this.props;
-      newTodo(todoIt);
-      todoIt.title = "";
-      todoIt.id = Math.random() * 1000;
+      let { id } = this.state;
+      id = Math.random();
+      newTodo(newTodos);
+
       this.setState({
-        todoIt: todoIt,
+        title: "",
+        id: id,
       });
     }
   }
 
   render() {
-    const { todoIt } = this.state;
+    const { title } = this.state;
     return (
       <>
         <input
           type="text"
           className="form_input"
-          value={todoIt.title}
+          value={title}
           onChange={this.handleInput}
           onKeyPress={this.submitTitle}
           placeholder="Write what you have to do an press Enter key"
