cc6dd1b4fea8f1db8a6592963aaa1721ad699278
Jonathan Cedeño <jony.cede@gmail.com>
2021-05-21 11:21:02 +0200

    implemented icons dark and light mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,32 +2,24 @@ import React from "react";
 
 import "./Header.scss";
 
+const MOON_PATH = "./img/moon.png";
+const SUN_PATH = "./img/sun.png";
+
 function Header({ toggleDarkLightMode, isDark }) {
-  let state;
+  let mode;
   if (isDark === true)
-    state = (
-      <button
-        className="without-border without-background"
-        type="button"
-        onClick={toggleDarkLightMode}
-      >
-        LIGHT
-      </button>
-    );
-  else
-    state = (
-      <button
-        className="without-border without-background"
-        type="button"
-        onClick={toggleDarkLightMode}
-      >
-        DARK
-      </button>
-    );
+    mode = <img className="icon-mode" src={SUN_PATH} alt="Sun" />;
+  else mode = <img className="icon-mode" src={MOON_PATH} alt="Moon" />;
   return (
     <div className="d-flex flex-row justify-content-between">
       <div>T O D O</div>
-      {state}
+      <button
+        className="without-border without-background"
+        type="button"
+        onClick={toggleDarkLightMode}
+      >
+        {mode}
+      </button>
     </div>
   );
 }
