34da298b8d50acb505b2c5d286eafcf16454cc7d
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-05-21 19:15:42 +0200

    preparing routes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.scss";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -7,7 +8,9 @@ function Footer() {
       <h6>5 items left</h6>
       <h6>All</h6>
       <h6>Active</h6>
-      <h6>Completed</h6>
+      <Link to="/complete">
+        <h6>Completed</h6>
+      </Link>
       <h6>Clear Completed</h6>
     </footer>
   );
---
+++ src/components/pages/complete/comletepage.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import Todo from "../../Todo";
+
+function Complete() {
+  return <p>hola ke tal</p>;
+}
+
+export default Complete;
