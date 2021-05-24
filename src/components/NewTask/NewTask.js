faef38e7bd3eb9fd56eaa09677c1dc377acfde6b
Jonathan Cedeño <jony.cede@gmail.com>
2021-05-24 23:41:47 +0200

    style of NewTask component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -4,6 +4,8 @@ import { Formik } from "formik";
 
 import taskSchema from "./task-schema";
 
+import "./NewTask.scss";
+
 function addDetailsNewTask(data) {
   return {
     id: uuid(),
@@ -35,37 +37,54 @@ function NewTask({ saveNewTask }) {
           touched,
         }) => (
           <form
-            className="d-flex flex-row justify-content-around"
+            className="d-flex flex-row align-items-center form-wrapper"
             onSubmit={handleSubmit}
           >
-            <input
-              id="isCompleted"
-              name="isCompleted"
-              type="checkbox"
-              value={values.isCompleted}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <div className="form-group">
+            <div className="d-flex justify-content-center align-items-center isCompleted-wrapper">
+              <input
+                id="isCompleted"
+                name="isCompleted"
+                type="checkbox"
+                value={values.isCompleted}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="isCompleted-class"
+              />
+            </div>
+            <div className="d-flex flex-row justify-content-between text-input-wrapper">
               <input
                 className={
                   touched.title && errors.title
-                    ? "form-control is-invalid"
-                    : "form-control"
+                    ? "text-input is-invalid"
+                    : "text-input"
                 }
                 id="title"
                 name="title"
                 type="text"
-                placeholder="Insert a new task..."
+                placeholder="Insert a new task ..."
                 value={values.title}
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
               {touched.title && errors.title && (
-                <p className="invalid-feedback">{errors.title}</p>
+                <p className="text-input-failed">{errors.title}</p>
               )}
+              <button className="submit-new-task" type="submit">
+                {touched.title && errors.title ? (
+                  <img
+                    className="icon-submit"
+                    src="./img/fail.png"
+                    alt="Fail"
+                  />
+                ) : (
+                  <img
+                    className="icon-submit"
+                    src="./img/enter.png"
+                    alt="Correct"
+                  />
+                )}
+              </button>
             </div>
-            <button type="submit" />
           </form>
         )}
       </Formik>
