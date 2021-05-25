5618e8812609662b4db5136a6f1721df8f032c1f
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-05-25 09:42:50 +0200

    last details functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ import Completed from "./components/pages/Complete";
 
 import "./_App.scss";
 
+const LOCAL_STORAGE_KEY = "react-sc-state";
+
+function loadLocalStorage() {
+  const json = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  const state = JSON.parse(json);
+
+  if (!state) {
+    return {
+      todos: [],
+    };
+  }
+
+  return state;
+}
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +32,18 @@ class App extends Component {
     this.handleDelete = this.handleDelete.bind(this);
     this.handleSelected = this.handleSelected.bind(this);
     this.clear = this.clear.bind(this);
+  }
+
+  componentDidMount() {
+    const prevState = loadLocalStorage();
+
+    this.setState({
+      todos: prevState.todos,
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.state));
   }
 
   handleSelected(arry, item) {
