09771cfc22666392c77850b90a2c2df98456b01c
sergi92 <45389112+sergi92@users.noreply.github.com>
2021-09-21 12:16:10 +0200

    delete button complete

--- src/components/DeleteButton/DeleteButton.js
+++ src/components/DeleteButton/DeleteButton.js
@@ -1,1 +1,22 @@
 export default function DeleteButton() {}
+
+class DeleteButton extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            listToDelete: [],
+        }
+    };
+
+        deleteItem(item){
+            const list = this.state.listToDelete.filter(i => i.id !== item.id)
+            this.setState({list})
+        }
+    
+    render(){
+        return <button className="delete-button" onClick={(this.deleteItem(item))} >
+            X
+            </button>
+    }
+}
