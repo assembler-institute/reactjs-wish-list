c928fa7b28350419102fadaea5567224d6d322cb
AranBeitia <arantza.beitia@gmail.com>
2021-09-21 15:15:31 +0200

    change html

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ function App() {
   return (
     <div className="position-relative">
       <section className="padding-15 mb-4 bg-image-hero"></section>
-      <section className="container">
-        <div className="card position-absolute absolute-center">
-          <header className="card-header">
-            <h1>TODO</h1>
-            <input type="text" placeholder="insert text here" />
-          </header>
-          <main className="card-body">
+      <main className="card position-absolute absolute-center">
+        <header className="card-header">
+          <h1>TODO</h1>
+        </header>
+        <form className="card-body">
+          <input type="text" placeholder="insert text here" />
           <ul className="list-group list-group-flush">
             <li className="list-group-item is-disabled" aria-disabled="true">
               <input type="checkbox" name="name1" id="" />
@@ -29,21 +28,20 @@ function App() {
               <label htmlFor="name4">(lo que venga del input de arriba)</label>
             </li>
           </ul>
-          </main>
-          <footer className="card-footer text-muted d-flex justify-content-between align-items-center">
-            <span className="text-tag">5 items left</span>
-            <div className="d-flex">
-              <span className="text-tag">All</span>
-              <span className="text-tag px-2">Active</span>
-              <span className="text-tag">Completed</span>
-            </div>
-            <span className="text-tag">Clear Completed</span>
-          </footer>
-        </div>
-        <footer className="text-center">
-          <p>Drag and drop to reorder list</p>
+        </form>
+        <footer className="card-footer text-muted d-flex justify-content-between align-items-center">
+          <span className="text-tag">5 items left</span>
+          <div className="d-flex">
+            <span className="text-tag">All</span>
+            <span className="text-tag px-2">Active</span>
+            <span className="text-tag">Completed</span>
+          </div>
+          <span className="text-tag">Clear Completed</span>
         </footer>
-      </section>
+      </main>
+      <footer className="text-center">
+        <p>Drag and drop to reorder list</p>
+      </footer>
     </div>
   );
 }
