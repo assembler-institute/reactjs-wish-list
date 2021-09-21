dc5d46f0361b12c32baa6ba8e69e834089397871
Haroon Ahmad <haroon334@gmail.com>
2021-09-21 13:30:59 +0200

    Add todo to Local Storage

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,7 +57,7 @@ module.exports = {
   ],
   rules: {
     "prettier/prettier": [
-      "error",
+      "off",
       {
         endOfLine: "auto",
       },
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   return (
     <main className="container mt-5">
       <section className="row">
-        <div className="col col-12">
-          <h1>Hola mundo</h1>
+        <div className="row row-12">
+          <NewTodo />
         </div>
       </section>
     </main>
