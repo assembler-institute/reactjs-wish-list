ba0bb3e0914cde6a2d896a6d269ff68f369f6e38
andarbech <andarbech1.com>
2021-09-21 15:08:31 +0200

    prueba

--- src/Components/Input/input.js
+++ src/Components/Input/input.js
@@ -3,11 +3,26 @@ import "./input.scss";
 
 
 class Input extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+        }
+    }
+    componentDidMount() {
+        //meterlo dentro del local storage
+    }
+
+
     render(){
         return(
             <div className="input-group mb-3">
                 <span className="input-group-text" id="basic-addon1">@</span>
-                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
+                <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Username" 
+                aria-label="Username" 
+                ></input>
             </div>
         )
     }
