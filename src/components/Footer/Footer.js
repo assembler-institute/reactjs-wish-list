1bbc0cc83fead3736cd2117ab1de7021ff239481
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-09-21 11:53:35 +0200

    Main Footer Structure

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import { React, Component } from "react";
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer__left">X items left</div>
+        <div className="footer__buttons">
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+        <button>Clear Completed</button>
+      </div>
+    );
+  }
+}
+
+export default Footer;
