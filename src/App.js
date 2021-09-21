bb1ba6e71162aac3b1bf1886a9517a7a0746d236
sanadriu <skualed@gmail.com>
2021-09-21 13:57:27 +0200

    App component brainstorm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,94 @@
-import React from "react";
-import CheckBox from "./components/CheckBox";
+import React, { Component } from "react";
+import { v4 as uuid } from "uuid";
 
-function App() {
-  return (
-    <main className="container mt-5">
-      <section className="row">
-        <div className="col col-12">
-          <h1>Hola mundo</h1>
-          <CheckBox />
-        </div>
-      </section>
-    </main>
-  );
+import TodoCreate from "./components/TodoCreate";
+import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: "9c34e805-7bfc-401a-b386-468c25315e46",
+          text: "todo 01",
+          done: false,
+          isEditing: false,
+        },
+        {
+          id: "d733a37e-cc4e-4cde-916f-935b3e915bb3",
+          text: "todo 02",
+          done: false,
+          isEditing: false,
+        },
+      ],
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  addTodo(data) {
+    const { todos } = this.state;
+
+    todos.push({
+      ...data,
+      id: uuid(),
+    });
+
+    this.setState((prevState) => ({
+      ...prevState,
+      todos: newTodos,
+    }));
+  }
+
+  deleteTodo(id) {
+    const { todos } = this.state;
+
+    const newTodos = todos.filter((item) => item.id !== id);
+
+    this.setState((prevState) => ({
+      ...prevState,
+      todos: newTodos,
+    }));
+  }
+
+  setDoneTodo(id) {
+    const { todos } = this.state;
+
+    const newTodos = todos.map((item) => {
+      if (item.id === id) item.done = !item.done;
+
+      return item;
+    });
+
+    this.setState((prevState) => ({
+      ...prevState,
+      todos: newTodos,
+    }));
+  }
+
+  render() {
+    return (
+      <main className="container mt-5">
+        <section className="row">
+          <div className="col col-12">
+            <h1>TODO</h1>
+            <TodoCreate
+              handleSubmit={addTodo}
+              handleDelete={deleteTodo}
+              handleSetDone={setDoneTodo}
+            />
+            <div>
+              <TodoList todos={this.state.todos} />
+              <Footer count={this.state.todos.length} />
+            </div>
+          </div>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
