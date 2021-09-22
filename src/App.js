8bd3eac01e2f82b6ba1fb74f325a8977ed4c6828
AntonioCopete <antcopjim@gmail.com>
2021-09-22 16:22:57 +0200

    added add task warning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from "react";
 import Background from "./components/Background";
 import ChangeMode from "./components/ChangeMode";
-import NewTodo from "./components/NewTodo"
-import TodoList from "./components/TodoList"
-import Menu from "./components/Menu"
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+import Menu from "./components/Menu";
 import "./App.scss";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
 
-  
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
 
-  // handleInput() {
-  //   const 
-  // }
+  handleAddTodo(event) {
+    let task;
+    if (event.key === "Enter") {
+      task = event.target.value;
+      let newToDo = {
+        id: new Date().getTime(),
+        task: task,
+        completed: false,
+      };
 
+      this.setState((prevState) => ({
+        todos: [...prevState.todos, newToDo],
+      }));
+
+      event.target.value = null;
+    }
+  }
 
   render() {
+    const { todos } = this.state;
     return (
       <>
         <Background />
@@ -27,11 +47,25 @@ class App extends React.Component {
                 <ChangeMode />
               </header>
               <div>
-                <NewTodo />
-                <div className="list-container bg-white shadow">
-                  <TodoList todos={[]} />
-                  <Menu />
-                </div>
+                <input
+                  onKeyDown={this.handleAddTodo}
+                  id="inputNewToDo"
+                  type="text"
+                  className="w-100 mt-4 new-todo shadow"
+                ></input>
+                {todos.length > 0 ? (
+                  <div className="list-container bg-white shadow">
+                    <TodoList todos={todos} />
+                    <Menu />
+                  </div>
+                ) : (
+                  <div className="list-container bg-white shadow mt-4 p-2 ">
+                    <h4 className="m-0 text-center text-danger">
+                      Create a new task please
+                    </h4>
+                  </div>
+                )}
+                {}
               </div>
             </div>
           </section>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,19 +4,10 @@ import "./TodoList.scss"
 
 export default class TodoList extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {
-      todos: [{id: 1, task: "Tarea 1", completed: false}, {id: 2, task: "Tarea 2", completed: false}]
-    }
-    this.addToDo = this.addToDo.bind(this)
+    super(props);
   }
-
-  addToDo() {
-
-  }
-
   render() {
-    const { todos } = this.state
+    const { todos } = this.props
     return (
       <ul className="w-100 mt-4 bg-white todo-list">
         {todos.map(todo => {
