d459b8484d4b225526c18ccc4be6afc69279022a
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-09-22 13:58:11 +0200

    new task styles

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -14,34 +14,34 @@ function Task({
       {(draggableProvided) => (
         <li
           key={index}
-          className="card card-body mt-2"
+          className="card card-body mt-2 d-flex flex-row"
           {...draggableProvided.draggableProps}
           ref={draggableProvided.innerRef}
           {...draggableProvided.dragHandleProps}
         >
           <button
+            className="btn btn-secondary"
+            style={{ backgroundColor: task.done ? 'black' : 'white' }}
+            onClick={(e) => toggleDoneTask(e, task.id)}
+          />
+          <button
+            className="w-100 text-left pl-3"
             style={{
-              textDecoration: task.done ? "line-through" : "",
-              display: task.isEditing ? "none" : "block",
+              textDecoration: task.done ? 'line-through' : '',
+              display: task.isEditing ? 'none' : 'block'
             }}
-            onClick={(e) => toggleEditTask(e, task.id)}
-          >
+            onClick={(e) => toggleEditTask(e, task.id)}>
             {task.text}
           </button>
           <input
+            className="w-100 text-left pl-3"
             type="text"
-            style={{ display: task.isEditing ? "block" : "none" }}
+            style={{ display: task.isEditing ? 'block' : 'none' }}
             value={task.text}
-            onChange={(e) => saveEditTask(e, task.id)}
+            onChange={e => saveEditTask(e, task.id)}
             onKeyDown={(e) => onKeyDownEdit(e, task.id)}
             autoFocus
           />
-          <button
-            className="btn btn-secondary"
-            onClick={(e) => toggleDoneTask(e, task.id)}
-          >
-            {task.done ? "Done" : "Todo"}
-          </button>
           <button
             className="btn btn-secondary"
             onClick={(e) => removeTask(e, task.id)}
