696b05519f7ce061a10b2bfb72c1bb01f0b741e7
Enrique Hidalgo <koko.freelance@gmail.com>
2021-09-22 13:09:39 +0100

    More fighting

--- src/components/EditButtons/RemoveButton.jsx
+++ src/components/EditButtons/RemoveButton.jsx
@@ -1,24 +1,19 @@
-/* eslint-disable prettier/prettier */
 /* eslint-disable react/prefer-stateless-function */
 import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import styles from "./RemoveButton.module.scss";
 
-
-export default class RemoveButton extends React.Component {
-  DeleteTaskHandler = ({ toDoItem, setToDoItem }) => {
-    setToDoItem(toDoItem.filter((item) => item.id !== toDoItem.id));
-    console.log("click");
+export default function RemoveButton({ toDoItem, setToDoItem }) {
+  const removeHandler = () => {
+    setToDoItem(toDoItem.filter((el) => el.id !== toDoItem.id));
   };
-
-  render() {
-    return <button
-      onClick={this.DeleteTaskHandler}
+  return (
+    <button
+      onClick={removeHandler}
       type="button"
       className={styles.removeButton}
-
     >
       <FaTrashAlt />
-    </button>;
-  }
+    </button>
+  );
 }
