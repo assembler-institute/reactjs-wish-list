f2af3f541d41ea438bdfde67bee6c72122257ea3
Daniella Alamo <dalamov08@gmail.com>
2021-09-22 21:12:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     }
   
     handleChange(e) {
-      this.setState({ todoName: e.target.value });
+      // this.setState({ todoName: e.target.value });
     }
 
     render(){
