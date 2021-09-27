270011e1089adb1f118609806fb17e02fbee2210
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-09-27 12:49:06 +0100

    style minor changes

--- src/components/TodoTask/TodoTask.js
+++ src/components/TodoTask/TodoTask.js
@@ -1,66 +1,86 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
-import { saveItem } from '../../utils/localStorage'
+import React from "react";
+import { saveItem } from "../../utils/localStorage";
 
-import "./TodoTask.scss"
+import "./TodoTask.scss";
 
 export default class TodoTask extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
-    const {task} = this.props
+    const { task } = this.props;
 
     this.state = {
       id: task.id,
       done: task.done,
       isEditing: task.isEditing,
-      inputValue: task.inputValue
-    }
+      inputValue: task.inputValue,
+    };
   }
 
   completeHandler = () => {
-    const {done} = this.state
-    !done ? 
-    ( saveItem({...this.state, done: true}) , this.setState({done: true}) ) 
-    : 
-    ( saveItem({...this.state, done: false}) , this.setState({done: false}) ) 
-  }
+    const { done } = this.state;
+    !done
+      ? (saveItem({ ...this.state, done: true }), this.setState({ done: true }))
+      : (saveItem({ ...this.state, done: false }),
+        this.setState({ done: false }));
+  };
 
   handlerSubmit = (event) => {
-    event.preventDefault()
-    saveItem({...this.state, isEditing: false})
-    this.setState({isEditing: false})
-  }
+    event.preventDefault();
+    saveItem({ ...this.state, isEditing: false });
+    this.setState({ isEditing: false });
+  };
 
   editHandler = (event) => {
-    this.setState({inputValue: event.target.value})
-  }
+    this.setState({ inputValue: event.target.value });
+  };
 
   render() {
+    const { inputValue, id, isEditing, done } = this.state;
+    const { handlerDeleteTask } = this.props;
 
-    const {inputValue, id, isEditing, done} = this.state
-    const {handlerDeleteTask} = this.props
+    if (id === "") return null;
 
-    if (id === '') return null
-
-    return(
-      <li className={`shadow-sm list-group-item d-flex flex-wrap justify-content-between align-items-center ${isEditing ? 'editing' : ''} ${done ? 'done' : ''}`}>
-          <button className="btn btn-light" type="button" onClick={this.completeHandler}>
-            <span role="img" aria-label='Check to complete'>
-              {done ? "❌" : "✅"}
-            </span>
-          </button>
-          <form onSubmit={this.handlerSubmit}>
-            <input required className="input-task" type="text" key={id} id={id} value={inputValue} 
-            onChange={this.editHandler} 
-            onFocus={() => this.setState({isEditing: true})} 
-            onBlur={() => this.setState({isEditing: false})} 
-            />
-          </form>
-          <button className="btn btn-light" type="button" onClick={() => handlerDeleteTask(id)}><span role="img" aria-label="trash bin">🗑️</span></button>
+    return (
+      <li
+        className={`shadow-sm list-group-item d-flex flex-wrap justify-content-between align-items-center ${
+          isEditing ? "editing" : ""
+        } ${done ? "done" : ""}`}
+      >
+        <button
+          className="btn btn-light"
+          type="button"
+          onClick={this.completeHandler}
+        >
+          <span role="img" aria-label="Check to complete">
+            {done ? "❌" : "✅"}
+          </span>
+        </button>
+        <form onSubmit={this.handlerSubmit}>
+          <input
+            required
+            title={inputValue}
+            className="input-task"
+            type="text"
+            key={id}
+            id={id}
+            value={inputValue}
+            onChange={this.editHandler}
+            onFocus={() => this.setState({ isEditing: true })}
+            onBlur={() => this.setState({ isEditing: false })}
+          />
+        </form>
+        <button
+          className="btn btn-light"
+          type="button"
+          onClick={() => handlerDeleteTask(id)}
+        >
+          <span role="img" aria-label="trash bin">
+            🗑️
+          </span>
+        </button>
       </li>
-      
-    )
+    );
   }
 }
--- src/pages/Completed/Completed.js
+++ src/pages/Completed/Completed.js
@@ -2,24 +2,21 @@
 import React from "react";
 import TodoList from "../../components/TodoList";
 
-
 export default class Completed extends React.Component {
-
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
-
   render() {
-
-    const {tasks} = this.props
-    const completedTasks = tasks.filter(task => task.done)
+    const { tasks } = this.props;
+    const completedTasks = tasks.filter((task) => task.done);
 
     return (
       <>
+        ((
         <TodoList {...this.props} tasks={completedTasks} />
       </>
-    )
+    );
   }
 }
