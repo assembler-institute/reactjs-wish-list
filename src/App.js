318ca630a1c51218b9a7087eb888b32562fb269c
Josep Romero Ros <joseprrr@gmail.com>
2021-09-27 16:17:04 +0200

    hidden btn remove

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React, { Component } from "react";
-import { Route, Switch} from "react-router-dom";
 import Home from './Home'
 
 class App extends Component {
 
   render(){
     return (
-      <>
-      <Switch>
-
-      <Route
-          path="/"
-          exact
-          render={(routeProps) => (
-          <Home 
-            {...routeProps}
-          />
-            )}/>
-      <Route
-          path="/Actives"
-          exact
-          render={(routeProps) => (
-            <TodoList
-            {...routeProps}
-            items={this.state.listToDos}
-                    handleRemove={this.deleteItem}
-                    handelEdit={this.editItem}
-            />)}/>
-             <Route
-          path="/Done"
-          exact
-          render={(routeProps) => (
-            <TodoList
-            {...routeProps}
-            items={this.state.listToDos}
-
-                    handleRemove={this.deleteItem}
-                    handelEdit={this.editItem}
-            />)}/>
-            </Switch>
-      </>
+      <Home/>
     )
     }
 }
