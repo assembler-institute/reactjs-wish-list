53ef027bf31fd1ad415fbd68906a111f5cb4f81e
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-09-27 10:04:40 +0100

    background+title

--- src/App.js
+++ src/App.js
@@ -8,73 +8,87 @@ import Header from "./components/Header";
 import MainList from "./components/MainList";
 import FilterToDo from "./components/FilterToDo/FilterToDo";
 import { deleteItem, saveItem } from "./utils/localStorage";
-
+import Background from "./components/Background/Background";
 
 export default class App extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      tasks: []
-    }
-
+      tasks: [],
+    };
   }
-  
+
   componentDidMount() {
     // Then re-render MainList (props: task) & Footer (props: lenght)
-    this.refreshState()
+    this.refreshState();
   }
 
   handlerNewToDo = (task) => {
     // Save Todo en localStorage
-    saveItem(task)
+    saveItem(task);
     // Refresh state with new localStorage
-    this.refreshState()
-  } 
-  
+    this.refreshState();
+  };
+
   handlerDeleteTask = (id) => {
     // Delete item from locaStorage
-    deleteItem(id)
+    deleteItem(id);
 
     // Refresh state
-    this.refreshState()
-  }
+    this.refreshState();
+  };
 
   handlerClearCompleted = () => {
-    Object.values(localStorage).forEach(elm => {
-      const task = JSON.parse(elm)
-      task.done ? deleteItem(task.id) : null
-    })
-    this.refreshState()
-  }
+    Object.values(localStorage).forEach((elm) => {
+      const task = JSON.parse(elm);
+      task.done ? deleteItem(task.id) : null;
+    });
+    this.refreshState();
+  };
 
   refreshState = () => {
     // Get items from localStorage
-    const localStorageTasks = Object.values(localStorage).map(elm => JSON.parse(elm))
+    const localStorageTasks = Object.values(localStorage).map((elm) =>
+      JSON.parse(elm),
+    );
     // Refresh state with new localStorage
-    localStorageTasks.length > 0 ? this.setState({tasks: [...localStorageTasks]}) : this.setState({tasks: []})
-  }
+    localStorageTasks.length > 0
+      ? this.setState({ tasks: [...localStorageTasks] })
+      : this.setState({ tasks: [] });
+  };
 
   render() {
-    const {tasks} = this.state
+    const { tasks } = this.state;
 
     return (
-      <main className="container mt-5">
-        <section className="row">
-          <div className="col col-12">
-            <Header>
-              <AddToDoTask handlerNewToDo={this.handlerNewToDo}/>
-            </Header>
-            <BrowserRouter>
-              <MainList tasks={tasks} handlerDeleteTask={this.handlerDeleteTask} />
-              <Footer>
-                <FilterToDo counter={tasks.length} handlerClearCompleted={this.handlerClearCompleted}/>
-              </Footer>
-            </BrowserRouter>
-          </div>
-        </section>
-      </main>
-    )
+      <>
+        <Background />
+        <main id="main-app" className="container mt-5">
+          <h1 className="title display-1 text-white text-center m-5 font-weight-bold">
+            <strong>To Do List</strong>
+          </h1>
+          <section className="row justify-content-center">
+            <div className="col col-6 p-5 rounded bg-white">
+              <Header>
+                <AddToDoTask handlerNewToDo={this.handlerNewToDo} />
+              </Header>
+              <BrowserRouter>
+                <MainList
+                  tasks={tasks}
+                  handlerDeleteTask={this.handlerDeleteTask}
+                />
+                <Footer>
+                  <FilterToDo
+                    counter={tasks.length}
+                    handlerClearCompleted={this.handlerClearCompleted}
+                  />
+                </Footer>
+              </BrowserRouter>
+            </div>
+          </section>
+        </main>
+      </>
+    );
   }
 }
---
+++ src/components/Background/Background.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+
+import "./Background.scss";
+
+export default class Background extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="background_img" />;
+  }
+}
