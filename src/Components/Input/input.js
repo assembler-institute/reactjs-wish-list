e82dd83685fd5ff19f6a2a2f0b6db1a7fb6ae5a3
andarbech <andarbech1.com>
2021-09-29 09:25:15 +0200

    filter bugs

--- src/Components/Input/input.js
+++ src/Components/Input/input.js
@@ -1,31 +1,30 @@
 import React, { Component } from "react";
 import "./input.scss";
 
-
 class Input extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        inputValue: "",
+      newValue: "",
     };
-}
+  }
 
   handleChange = (e) => {
     this.setState({
-    inputValue: e.target.value,
-});
-};
+      newValue: e.target.value,
+    });
+  };
   handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      this.props.handleEnter(this.state.inputValue);
+      this.props.handleEnter(this.state.newValue);
       this.setState({
-        inputValue: ""
+        newValue: "",
       });
     }
   };
 
   render() {
-    const { inputValue } = this.state;
+    const { newValue } = this.state;
     return (
       <>
         <div className="input-group mb-3">
@@ -34,12 +33,11 @@ class Input extends Component {
             placeholder="Insert any activity... "
             className="input__text"
             aria-label="Insert any activity..."
-            value={inputValue}
+            value={newValue}
             onChange={this.handleChange}
             onKeyDown={this.handleKeyDown}
           />
-          <div>
-          </div>
+          <div></div>
         </div>
       </>
     );
--- src/Home/home.js
+++ src/Home/home.js
@@ -1,108 +1,90 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router,
-          Switch,
-          Route,
- } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Input from "../Components/Input";
-import TodoList from "../Components/TodoList"
-import Footer from "../Components/Footer"
+import TodoList from "../Components/TodoList";
+import Footer from "../Components/Footer";
 import "./home.scss";
 import TodoListCompleted from "../Components/TodoList/TodoList_Completed";
 import TodoListActive from "../Components/TodoList/TodoList_Active";
 
-
-
-
-
 class Home extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            listToDos: [],
-            newToDo:"",
-            done:false
-        };
-        this.addItem = this.addItem.bind(this);
-         this.editItem=this.editItem.bind(this)
-        this.deleteItem=this.deleteItem.bind(this)
-      }
-      // addName = (value,done) => {
-      //   console.log({ value });
-      //   this.setState({
-      //     list: [...this.state.list, value]
-      //   });
-      //};
-        addItem(value) {
-        const newToDo = {
-        id: Math.floor(Math.random()*100),
-        value:value,
-        
-        }
-        const{listToDos}=this.state
-        listToDos.push(newToDo);
-        this.setState({
-        listToDos,        
-        }); 
-      }; 
-    editItem(id, value, done){
-      const{listToDos}=this.state
-      listToDos.find((item) =>{
-        if(item.id===id){
-        item.done=!done
-          }
-        });
-        this.setState({
-          listToDos:listToDos,
-          }); 
+  constructor(props) {
+    super(props);
+    this.state = {
+      listToDos: [],
+      newToDo: "",
+      done: false,
     };
-
-    deleteItem(id) {
-      const{listToDos}=this.state
-      const updatedList = listToDos.filter(item => item.id !== id);
-      this.setState({ 
-      listToDos: updatedList,
-      }); 
+    this.addItem = this.addItem.bind(this);
+    this.editItem = this.editItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
-/*   toDoDone() {
-    const{listToDos}=this.state
-  const done= listToDos.filter((item) => console.log(item))
-  } */
-  render(){
+  addItem(value,done) {
+    const newToDo = {
+      id: Math.floor(Math.random() * 100),
+      value: value,
+      done:done
+    };
+    const { listToDos } = this.state;
+    listToDos.push(newToDo);
+    this.setState({
+      listToDos,
+    });
+  }
+  editItem(id, value, done) {
+    const { listToDos } = this.state;
+    listToDos.find((item) => {
+      if (item.id === id) {
+        item.done = !done;
+      }
+    });
+    this.setState({
+      listToDos: listToDos,
+    });
+  }
+
+  deleteItem(id) {
+    const { listToDos } = this.state;
+    const updatedList = listToDos.filter((item) => item.id !== id);
+    this.setState({
+      listToDos: updatedList,
+    });
+  }
+  render() {
+    const done=this.state.newToDo
+    console.log(done);
     return (
       <Router>
-      <main className="container mt-5">
-        <div className="container-lg">
-          <h1 className="title">TO DO</h1>
-        </div>
-       
+        <main className="container mt-5">
+          <div className="container-lg">
+            <h1 className="title">TO DO</h1>
+          </div>
           <section className="row container-lg input__todo">
-            <Input handleEnter={this.addItem}
-                    handelEdit={this.editItem}
-          />
-            </section>
+            <Input handleEnter={this.addItem} handelEdit={this.editItem} />
+          </section>
           <section className="row container-lg">
             <Switch>
               <Route path="/" exact>
-                <TodoList 
-                items={this.state.listToDos}
-                handleRemove={this.deleteItem}
-                handelEdit={this.editItem} />
-              </Route>
-              <Route path="/active" exact>
-                <TodoListActive 
+                <TodoList
+                  items={this.state.listToDos}
+                  handleRemove={this.deleteItem}
+                  handelEdit={this.editItem}
                 />
               </Route>
+              <Route path="/active" exact>
+                <TodoListActive />
+              </Route>
               <Route path="/completed" exact>
-                <TodoListCompleted  handleDone={this.toDoDone}/>
+                <TodoListCompleted handleDone={this.toDoDone} />
               </Route>
             </Switch>
             <Footer />
           </section>
         </main>
-        </Router>
-      );
-    }
+      </Router>
+    );
   }
+}
 
-  export default Home
+export default Home;
