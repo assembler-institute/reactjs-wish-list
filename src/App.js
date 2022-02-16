f3f6f22f9c708805746058f3ca9d1ed7e423efc3
LuisMM24 <luismateo2416@gmail.com>
2022-02-16 09:31:02 +0100

    background responsive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
 import TaskInput from "./components/TaskInput";
 import "./app.scss";
 
 function App() {
   return (
-    <main>
-      <Header />
-      <article>
-        <TaskInput />
-      </article>
-    </main>
+    <>
+      <header />
+      <main>
+        <h1 className="title">TODO</h1>
+        <article>
+          <TaskInput />
+        </article>
+      </main>
+    </>
   );
 }
 
--- src/components/Header/Header.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return <header>TODO</header>;
-}
