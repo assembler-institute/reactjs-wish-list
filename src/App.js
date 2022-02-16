e306ae06129c670ac8e614717c721a097d39c1b4
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-02-16 13:01:58 +0100

    delete & complete works

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       todos={todos} 
       setTodos={setTodos} 
       setInputText={setInputText} />
-      <TodoList todos={todos}/>
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,32 @@
 import React from 'react'
 
-function Todo({text}) {
-  return (
+function Todo ({text, todo, todos, setTodos}){
+    const deleteHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };    
+    const completeHandler = () => {
+        setTodos(todos.map(item =>{
+            if(item.id === todo.id){
+                return{
+                    ...item, completed: !item.completed,
+                };
+            }
+            return item;
+        })
+        );
+    };
+
+return (
     <div className="todo">
-        <li className="todo-item">{text}</li>
-        <button type="button" className="complete-btn">
+        <li className={`todo-item ${todo.completed ? "completed" : "" }`}>{text}</li>
+        <button onClick={completeHandler} type="button" className="complete-btn">
             <i className="fas fa-check" />
         </button>
-        <button  type="button" className="trash-btn">
+        <button onClick={deleteHandler} type="button" className="trash-btn">
             <i className="fas fa-trash" />
         </button>
     </div>
-  )
+)
 }
 
 export default Todo
