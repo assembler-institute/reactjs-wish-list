d8aee37299c70757822d6645a7fdc9f9c7b1a7de
iigunchev <iigunchev@gmail.com>
2022-02-18 13:32:17 +0100

    router wip

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 // import ToDoItem from "../ToDoItem";
 import "./ToDoList.scss";
@@ -15,37 +16,43 @@ export default function ToDoList({
   isEmpty,
   toggleEditing,
 }) {
-  return (
-    <div className="list-container">
-      {data.length > 0 && (
-        <ul className="todos-container" data-testid="todos-list">
-          {data.map((item) => (
-            <li key={item.id} data-testid="todo-item">
-              <ToDoItem
-                data={data}
-                id={item.id}
-                text={item.text}
-                done={item.done}
-                isEditing={item.isEditing}
-                emptyError={isEmpty}
-                toggleEditing={() => toggleEditing(item.id)}
-                handleDone={() => isCompleted(item.id)}
-                handleDelete={() => handleDelete(item.id)}
-                handleChangeUpdate={handleChangeUpdate}
-                handleKeyPressUpdate={handleKeyPressUpdate}
-                handleUpdate={() => handleUpdate(item.id)}
-              />
-            </li>
-          ))}
-        </ul>
-      )}
-      {!data.length && (
-        <div className="emptyTodosContainer" data-testid="no-todos">
-          <img src={noTodosImg} alt="" className="noTodos" />
-          <h3>Please write your first ToDo</h3>
-        </div>
-      )}
+  // const completedTodos = data.filter((item) => item.done === true);
+  // const activeTodos = data.filter((item) => item.done !== true);
 
+  return (
+    <BrowserRouter>
+      <Route path="/" exact>
+        <div className="list-container">
+          {data.length > 0 && (
+            <ul className="todos-container" data-testid="todos-list">
+              {data.map((item) => (
+                <li key={item.id} data-testid="todo-item">
+                  <ToDoItem
+                    data={data}
+                    id={item.id}
+                    text={item.text}
+                    done={item.done}
+                    isEditing={item.isEditing}
+                    emptyError={isEmpty}
+                    toggleEditing={() => toggleEditing(item.id)}
+                    handleDone={() => isCompleted(item.id)}
+                    handleDelete={() => handleDelete(item.id)}
+                    handleChangeUpdate={handleChangeUpdate}
+                    handleKeyPressUpdate={handleKeyPressUpdate}
+                    handleUpdate={() => handleUpdate(item.id)}
+                  />
+                </li>
+              ))}
+            </ul>
+          )}
+          {!data.length && (
+            <div className="emptyTodosContainer" data-testid="no-todos">
+              <img src={noTodosImg} alt="" className="noTodos" />
+              <h3>Please write your first ToDo</h3>
+            </div>
+          )}
+        </div>
+      </Route>
       <footer className="mt-auto" data-testid="app-footer">
         <span className="items-left">
           {data.length === 1
@@ -53,14 +60,14 @@ export default function ToDoList({
             : `${data.length} todos left`}
         </span>
         <div className="items-status">
-          <button type="button">All</button>
-          <button type="button">Active</button>
-          <button type="button">Completed</button>
+          <Link to="/">All</Link>
+          <Link to="/active">Active</Link>
+          <Link to="/completed">Completed</Link>
         </div>
         <button type="button" className="clear-completed">
           Clear Completed
         </button>
       </footer>
-    </div>
+    </BrowserRouter>
   );
 }
